136613d6fc5ee61c116815355a70413905756ffb
Sydney Bruce <sydneybruce20@gmail.com>
2024-01-05 09:37:34 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# This is a test of netlify forms with svelte
+
+This test attempts to solve the issue I am running into with my portfolio form .
+
+On form submit, the POST method is blocked by a 405 error. 
+
+I am going to try using [thislink](https://answers.netlify.com/t/netlify-forms-page-not-found-404-on-submit/36106/11) to assist with this issue
---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<h1> This is a forms test</h1> 
+
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		let myForm = document.getElementById('myform');
+		let formData = new FormData(myForm);
+		fetch('/', {
+			method: 'POST',
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
+			body: new URLSearchParams(formData).toString()
+		}).then(() => console.log('Form successfully submitted')).catch((error) =>
+			alert(error))
+	}
+</script>
+
+<form id="myform" on:submit{handleSubmit} name="hello" method="POST" data-netlify="true">
+	<p><label>Your Name: <input type="text" name="name" /></label></p>
+	<p><label>Your Email: <input type="email" name="email" /></label></p>
+	<p><label>Message: <textarea name="message"></textarea></label></p>
+	<p><button class="mx-2 bg-red-50 border-2 px-2 rounded-md  block mx-auto" type="submit">Send</button></p>
+	<br>
+	<input type="hidden" name="form-name" value="hello">
+</form>
