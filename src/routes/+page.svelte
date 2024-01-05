82de9ab06b869b87b8988554a32bef9df78458bb
Sydney Bruce <sydneybruce20@gmail.com>
2024-01-05 09:50:50 -0700

    changed form submit handler

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,20 @@
-<h1> This is a forms test</h1> 
-
-<script context="module">
-	export const prerender = true;
-</script>
-
 <script>
-	const handleSubmit = (e) => {
-		e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault()
 		let myForm = document.getElementById('myform');
 		let formData = new FormData(myForm);
 		fetch('/', {
-			method: 'POST',
+      method: 'POST',
 			headers: { "Content-Type": "application/x-www-form-urlencoded" },
 			body: new URLSearchParams(formData).toString()
 		}).then(() => console.log('Form successfully submitted')).catch((error) =>
-			alert(error))
+    alert(error))
 	}
 </script>
 
-<form id="myform" on:submit{handleSubmit} name="hello" method="POST" data-netlify="true">
+<h1> This is a forms test</h1> 
+
+<form id="myform" on:submit{handleSubmit()} name="hello" method="POST" data-netlify="true">
 	<p><label>Your Name: <input type="text" name="name" /></label></p>
 	<p><label>Your Email: <input type="email" name="email" /></label></p>
 	<p><label>Message: <textarea name="message"></textarea></label></p>
